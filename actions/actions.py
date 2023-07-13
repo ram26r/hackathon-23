f700138391f16daf1f311e3c43aa48bc44ed0a02
rajb06 <137992315+rajb06@users.noreply.github.com>
2023-07-13 06:56:18 +0530

    Update actions.py with commits, pipeline reports, pipelines

--- actions/actions.py
+++ actions/actions.py
@@ -62,3 +62,61 @@ class Get_Pipelines(Action):
 
         return []
 
+class Get_PipelineReport(Action):
+    def name(self) -> Text:
+        return "get_pipeline_report"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # Call your Python method here
+        result = get_pipeline_report()
+        print(result)
+        for item in result:
+            for x in item:
+                dispatcher.utter_message(text=str(x))
+            dispatcher.utter_message(text="\n")
+        # Use the dispatcher to send a message to the user
+
+        return []
+    
+    
+class Get_PipelineJobs(Action):
+    def name(self) -> Text:
+        return "get_pipeline_jobs"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # Call your Python method here
+        result = get_pipeline_jobs()
+        print(result)
+        for item in result:
+            for x in item:
+                dispatcher.utter_message(text=str(x))
+            dispatcher.utter_message(text="\n")
+        # Use the dispatcher to send a message to the user
+
+        return []
+    
+class Get_Commits(Action):
+    def name(self) -> Text:
+        return "get_commits"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # Call your Python method here
+        result = get_commits()
+        print(result)
+        for item in result:
+            for x in item:
+                dispatcher.utter_message(text=str(x))
+            dispatcher.utter_message(text="\n")
+        # Use the dispatcher to send a message to the user
+
+        return []
+
